5585702f0c85d1782b7b66f323560877a1bad925
Michael Crimando <crimandomichael@yahoo.com>
2025-07-29 10:02:43 -0400

    Init

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package com.scamofty
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
